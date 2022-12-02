fb08ce1f804ca9c1421e8be3b628be3f75e3e8c9
Leon <1395346178@qq.com>
2022-12-03 01:30:30 +0800

    main init

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import style from './index.module.scss'
+//仓库、酒馆、幸运转盘、藏身处、出发、图鉴（包括背景、身份）、设置
 export default function Main() {
   return (
     <div className={style.main}>
-      
+      <img className={style.bacPic} src="picture/bacdefault.jpg" alt="bacPic" />
+      <div className={style.darker}>
+
+          <div className={style.circleMain}></div>
+
+
+
+
+
+      </div>
+
     </div>
   )
 }
